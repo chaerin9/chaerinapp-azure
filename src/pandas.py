9333cc9300be2092c7d9f6b06ed1bfbc77b95df3
chaerin9 <106472616+chaerin9@users.noreply.github.com>
2022-05-30 12:52:44 +0900

    Update pandas.py

--- src/pandas.py
+++ src/pandas.py
@@ -1,45 +1,102 @@
-# -*- coding: utf-8 -*-
-
 import requests
+from bs4 import BeautifulSoup
 import json
-import logging
+import re
+from itertools import count
+from time import sleep
+import csv
 
-URL = "https://m.land.naver.com/complex/getComplexArticleList"
 
-param = {
-    'hscpNo': '19672',
-    'tradTpCd': 'A1',
-    'order': 'date_',
-    'showR0': 'N',
-}
+# land.naver.com '부산시 사하구' 검색 후 단지 정보 크롤링
 
-header = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.220 Whale/1.3.51.7 Safari/537.36',
-    'Referer': 'https://m.land.naver.com/'
-}
 
-logging.basicConfig(level=logging.INFO)
-page = 0
+def get_list():
 
-while True:
-    page += 1
-    param['page'] = page
+    pusan_dict = {
+        '강서구' : 2644000000,
+        '금정구' : 2641000000,
+        '기장군' : 2671000000,
+        '남구' : 2629000000,
+        '동구' : 2617000000,
+        '동래구' : 2626000000,
+        '부산진구' : 2623000000,
+        '북구' : 2632000000,
+        '사상구' : 2653000000,
+        '사하구' : 2638000000,
+        '서구' : 2614000000,
+        '수영구' : 2650000000,
+        '연제구' : 2647000000,
+        '영도구' : 2620000000,
+        '중구' : 2611000000,
+        '해운대구' : 2635000000,
+    }
 
-    resp = requests.get(URL, params=param, headers=header)
-    if resp.status_code != 200:
-        logging.error('invalid status: %d' % resp.status_code)
-        break
+    regionName_list = pusan_dict.keys()
+    regionCode_list = pusan_dict.values()
 
-    data = json.loads(resp.text)
-    result = data['result']
-    if result is None:
-        logging.error('no result')
-        break
+    reverse_dict = {v:k for k,v in pusan_dict.items()}
 
-    for item in result['list']:
-        if float(item['spc2']) < 80 or float(item['spc2']) > 85:
-            continue
-        logging.info('[%s] %s %s층 %s만원' % (item['tradTpNm'], item['bildNm'], item['flrInfo'], item['prcInfo']))
+    for code in regionCode_list:
 
-    if result['moreDataYn'] == 'N':
-        break
+        for page in count(1):
+            page_url = "http://land.naver.com/search/complexSearch.nhn"
+
+            params = {
+                'complexSearchSortOption':'popularRank.asc',
+                'cortarInfo':'[object Object]',
+                'regionAddress':'부산시 '+ reverse_dict[code],
+                'keywordInfo':'[object Object]',
+                'flashData':'[object Object]',
+                'flashMapRegionType':'region3',
+                'regionType':'region2',
+                'articleSizeTitle':'면적(㎡)',
+                'minSize':0,
+                'tradeType':'A1',
+                'query':'부산시',
+                'maxPrice':0,
+                'resultCode':'S00',
+                'isFirstSiteArticle':'true',
+                'maxSize':0,
+                'regionCode': code,
+                'isComplexRegion':'false',
+                'isaleComplexCount':0,
+                'complexCount':0,
+                'minPrice':0,
+                'complexSelectType':'all',
+                'trendPrice':'all',
+                'trendPriceDirectUse':'false',
+                'trendPriceDirectMax':'null',
+                'trendPriceDirectMin':'null',
+                'stationDistanceUse':'false',
+                'moveinDateUse':'false',
+                'householdUse':'false',
+                'page':page
+                }
+
+            print('################## '+ reverse_dict[code] +' page : {} ##################'.format(page))
+
+            r = requests.get(page_url, params=params)
+            r.encoding = "utf-8"
+            r = r.text
+            r_dict = json.loads(r)
+
+            # print(r_dict)
+
+            if r_dict['complexInfo']['complexList']:
+                r_list = r_dict['complexInfo']['complexList']
+
+                for i in range(len(r_list)):
+                    r_dict = r_list[i]
+                    data = r_dict['complexTypeName'] + "\t" + r_dict['address'] + "\t" + r_dict['complexName'] + "\n"
+
+                    with open('naver-land-'+ reverse_dict[code] +'.csv', 'a', newline='') as f:
+                        r_writer = csv.writer(f)
+                        r_writer.writerow([r_dict['complexTypeName'], r_dict['address'], r_dict['complexName']])
+
+                sleep(0.5)
+
+            else:
+                print("더 이상 자료 없음")
+                break
+
+get_list()
